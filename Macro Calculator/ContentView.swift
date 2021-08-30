2b5508d867847f9cff63ddf05e295ce6c90b15de
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-08-30 13:36:49 -0400

    Added basic input fields

--- Macro Calculator/ContentView.swift
+++ Macro Calculator/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var food = "Default"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Macro Calculator")
+                .padding()
+            HStack{
+                Text("Food: ")
+                    .padding()
+                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
+                
+            }
+            HStack{
+                Text("Fat: ")
+                    .padding()
+                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
+                
+            }
+            HStack{
+                Text("Carbs: ")
+                    .padding()
+                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
+                
+            }
+            HStack{
+                Text("Food: ")
+                    .padding()
+                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
+                
+            }
+            
+        }
+        
     }
 }
 
