d96dbda8d6e570b1356976c9178f3e1daf5a0627
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-08-30 11:50:48 -0400

    Initial Commit

---
+++ Macro Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Macro Calculator
+//
+//  Created by Vaaranan Yogalingam on 2021-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macro Calculator/Macro_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_CalculatorApp.swift
+//  Macro Calculator
+//
+//  Created by Vaaranan Yogalingam on 2021-08-30.
+//
+
+import SwiftUI
+
+@main
+struct Macro_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
