f61b0ebf2e3603aecf90755fb4bf33bda5b0f0b7
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-09-09 21:02:20 -0400

    Fixed Layout and added per and consumed quantity fields

--- Macro Calculator/ContentView.swift
+++ Macro Calculator/ContentView.swift
@@ -8,37 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var food = "Default"
+    @State var food : String = "Chicken"
+    @State var fat : String = "0.0"
+    @State var carbs : String = "0.0"
+    @State var protein : String = "0.0"
+    @State var calories : String = "0"
+    @State var perAmount : String = "0"
+    @State var consumedAmount : String = "0"
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading, spacing: -35.0){
+            // Title
             Text("Macro Calculator")
+                .font(.title)
                 .padding()
-            HStack{
-                Text("Food: ")
+            
+            // Food name input
+            HStack (spacing: 0.0){
+                Text("Food (100g Quantity): ")
                     .padding()
-                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
-                
-            }
-            HStack{
-                Text("Fat: ")
-                    .padding()
-                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
-                
-            }
-            HStack{
-                Text("Carbs: ")
-                    .padding()
-                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
-                
-            }
-            HStack{
-                Text("Food: ")
-                    .padding()
-                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center)
+                TextField("Enter Quantity", text: $food).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
                 
             }
             
+            // Fat quantity input
+            HStack{
+                Text("Fat: ")
+                    .padding()
+                TextField("Enter Quantity", text: $fat).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
+            
+            // Carbs quantity input
+            HStack{
+                Text("Carbs: ")
+                    .padding()
+                TextField("Enter Quantity", text: $carbs).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
+            
+            // Protein quantity input
+            HStack{
+                Text("Protein: ")
+                    .padding()
+                TextField("Enter Quantity", text: $protein).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
+            
+            // Calorie quantity input
+            HStack{
+                Text("Calories: ")
+                    .padding()
+                
+                TextField("Enter Quantity", text: $calories).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
+            HStack{
+                Text("Per (g): ")
+                    .padding()
+                
+                TextField("Enter Quantity", text: $perAmount).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
+            HStack{
+                Text("Amount Consumed (g): ")
+                    .padding()
+                
+                TextField("Enter Quantity", text: $consumedAmount).border(Color.black, width: 0.25).frame(width: 75, height: 100, alignment: .center).foregroundColor(.gray)
+                
+            }
         }
         
     }
